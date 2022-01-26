0dc79eeda506d105d878bd23ce22455c8d3713ca
GrafSoul <grafsoul@gmail.com>
2022-01-26 16:55:40 +0500

    feat: Updated Column component

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,12 +1,13 @@
-import { useDrop } from 'react-dnd';
 import { useRef } from 'react';
+import { useDrop } from 'react-dnd';
 import { ColumnContainer, ColumnTitle } from './styles';
 import { useAppState } from './state/AppStateContext';
 import { Card } from './Card';
 import { AddNewItem } from './AddNewItem';
 import { useItemDrag } from './utils/useItemDrag';
+
 import { isHidden } from './utils/isHidden';
-import { addTask, moveList } from './state/actions';
+import { addTask, moveTask, moveList, setDraggedItem } from './state/actions';
 
 type ColumnProps = {
 	text: string;
@@ -19,7 +20,7 @@ export const Column = ({ text, id, isPreview }: ColumnProps) => {
 	const tasks = getTasksByListId(id);
 	const ref = useRef<HTMLDivElement>(null);
 	const [, drop] = useDrop({
-		accept: 'COLUMN',
+		accept: ['COLUMN', 'CARD'],
 		hover() {
 			if (!draggedItem) {
 				return;
@@ -30,6 +31,16 @@ export const Column = ({ text, id, isPreview }: ColumnProps) => {
 				}
 
 				dispatch(moveList(draggedItem.id, id));
+			} else {
+				if (draggedItem.columnId === id) {
+					return;
+				}
+				if (tasks.length) {
+					return;
+				}
+
+				dispatch(moveTask(draggedItem.id, null, draggedItem.columnId, id));
+				dispatch(setDraggedItem({ ...draggedItem, columnId: id }));
 			}
 		},
 	});
